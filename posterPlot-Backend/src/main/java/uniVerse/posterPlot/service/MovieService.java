8087bb57c29fb51254496687df9ad995706b68ac
ej9374 <ej9374@naver.com>
2025-03-31 21:38:44 +0900

    fídocker 수정

--- posterPlot-Backend/src/main/java/uniVerse/posterPlot/service/MovieService.java
+++ posterPlot-Backend/src/main/java/uniVerse/posterPlot/service/MovieService.java
@@ -27,6 +27,7 @@ import uniVerse.posterPlot.repository.UserRepository;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -148,6 +149,9 @@ public class MovieService {
                         return Mono.error(new RuntimeException("Flask API 오류 발생: " + response.bodyToMono(String.class).block()));
                     })
                     .bodyToMono(ReceiveFlaskResponseDto.class)
+                    .retryWhen(reactor.util.retry.Retry.fixedDelay(30, Duration.ofSeconds(15))
+                            .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) ->
+                                    new RuntimeException("🚨 Flask API 재시도 실패: 최대 시도 횟수 초과")))
                     .block();
 
             if (flaskResponseDto == null){
