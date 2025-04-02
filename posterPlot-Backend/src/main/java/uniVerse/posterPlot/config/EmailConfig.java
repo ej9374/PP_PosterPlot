138be8158a51cdc4001a764d78151e3efd308b34
ej9374 <ej9374@naver.com>
2025-04-02 17:15:45 +0900

    email 비밀번호 수정

--- posterPlot-Backend/src/main/java/uniVerse/posterPlot/config/EmailConfig.java
+++ posterPlot-Backend/src/main/java/uniVerse/posterPlot/config/EmailConfig.java
@@ -16,7 +16,7 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");// 속성을 넣기 시작합니다. 이메일 전송에 사용할 SMTP 서버 호스트를 설정
         mailSender.setPort(587);// 587로 포트를 지정
         mailSender.setUsername("sexymestudying@gmail.com");//구글계정을 넣습니다.
-        mailSender.setPassword("oqaj nhbu wkxo qvlc");//구글 앱 비밀번호를 넣습니다.
+        mailSender.setPassword("wrwk uulq mvrq rouz");//구글 앱 비밀번호를 넣습니다.
 
         Properties javaMailProperties = new Properties();//JavaMail의 속성을 설정하기 위해 Properties 객체를 생성
         javaMailProperties.put("mail.transport.protocol", "smtp");//프로토콜로 smtp 사용
